b2a5368c9348d93bfa57d38bc6ce7f509250d5e9
rjcoll <robjcollier24@gmail.com>
2024-12-15 21:45:22 +0000

    adding signin button

--- components/signin-button.tsx
+++ components/signin-button.tsx
@@ -3,5 +3,5 @@
 import { signIn } from "next-auth/react"
  
 export default function SignIn() {
-  return <button onClick={() => signIn("discord")}>Sign In</button>
+  return <button className="inline-block px-6 mx-6 py-3 bg-indigo-600 text-white text-lg font-semibold rounded-lg shadow-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500" onClick={() => signIn("discord")}>Sign In</button>
 }
