d2b11b239e17d499f3ee51e8dd8889701b979132
Al Pagan <generamics@gmail.com>
2025-01-06 21:15:56 +0000

    Added check for loaded campaign data

--- components/Campaigns/Page.tsx
+++ components/Campaigns/Page.tsx
@@ -102,8 +102,8 @@ const Page = ({ user }: PageProps) => {
         </Card>
 
         <Heading size="4">Relevant documents</Heading>
-        <DocumentList campaignId={campaign.id} />
-        <AddDocument size="1" mt="2" campaignId={campaign.id}>
+        <DocumentList campaignId={campaign?.id} />
+        <AddDocument size="1" mt="2" campaignId={campaign?.id}>
           Add documents
         </AddDocument>
       </Container>
