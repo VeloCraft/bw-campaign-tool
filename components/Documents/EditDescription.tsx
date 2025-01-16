e7165ab8202c0ca11f5b6527f0b784f97f1eaccf
rjcoll <robjcollier24@gmail.com>
2025-01-16 21:35:39 +0000

    adding ability to edit document description

---
+++ components/Documents/EditDescription.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import Form from '@/components/Documents/Form';
+import useUpdateDoc from '@/hooks/useUpdateDoc';
+import useStatusUpdate from '@/hooks/useStatusUpdate';
+import { type ButtonProps } from '@radix-ui/themes';
+
+type EditProps = ButtonProps &
+  Partial<MediaRecord> & {
+    docId: string;
+  };
+
+const EditDescription = ({ docId, description, ...props }: EditProps) => {
+  const [onUpdate] = useUpdateDoc(`media/${docId}`, true);
+  const [open, setOpen] = React.useState(false);
+
+  const onAddMessage = useStatusUpdate();
+
+  const onSubmit = async (values: FormSubmission) => {
+    const newValues = { ...values, updatedAt: new Date() };
+    console.log(newValues);
+    await onUpdate(newValues);
+    onAddMessage({ message: 'Document updated', variant: 'success' });
+    setOpen(false);
+  };
+
+  const initialValues = {
+    description,
+  };
+
+  return (
+    <Form
+      data-testid="edit-description-button"
+      open={open}
+      setOpen={setOpen}
+      initialValues={initialValues}
+      onSubmit={onSubmit as any}
+      //disabled={loading}
+      title="Edit description"
+      description="Update the description of the document"
+      {...props}
+    />
+  );
+};
+
+export default EditDescription;
---
+++ components/Documents/Form.tsx
@@ -0,0 +1,45 @@
+import { Dialog, Button, type ButtonProps } from '@radix-ui/themes';
+import Form, { Field } from '@/components/Form';
+
+type ComponentProps = ButtonProps & {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  initialValues?: Partial<Campaign>;
+  onSubmit: (values: FormSubmission) => Promise<void>;
+  title: string;
+  description: string;
+};
+
+const Component = ({
+  open,
+  setOpen,
+  initialValues = {},
+  onSubmit,
+  title,
+  ...props
+}: ComponentProps) => {
+  return (
+    <Dialog.Root open={open} onOpenChange={setOpen}>
+      <Dialog.Trigger>
+        <Button {...props} />
+      </Dialog.Trigger>
+      <Dialog.Content>
+        <Dialog.Title>{title}</Dialog.Title>
+        <Form
+          variant="dialog"
+          onSubmit={onSubmit}
+          initialValues={initialValues}
+        >
+          <Field
+            label="Key details"
+            name="description"
+            type="textarea"
+            required
+          />
+        </Form>
+      </Dialog.Content>
+    </Dialog.Root>
+  );
+};
+
+export default Component;
